27666163d93d0160481c4f3e7dfa9c9862100ee5
Bereket <bmebrahtom@mum.edu>
2018-07-12 16:53:56 -0500

    Angular Exercise 2

---
+++ app/myvisibility.directive.spec.ts
@@ -0,0 +1,9 @@
+import { MyvisibilityDirective } from './myvisibility.directive';
+
+describe('MyvisibilityDirective', () => {
+  it('should create an instance', () => {
+    const directive = new MyvisibilityDirective();
+    expect(directive).toBeTruthy();
+    
+  });
+});
